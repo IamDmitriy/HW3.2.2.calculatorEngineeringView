1672422a58f55d638adedcdd2ff061456f227046
Dmitriy25 <ohapkin_da@mail.ru>
2020-02-12 11:06:36 +0300

    Корректировка ввода. Замена корневого layout на FrameLayout.

--- app/src/main/java/com/example/calculatorengineeringview/MainActivity.java
+++ app/src/main/java/com/example/calculatorengineeringview/MainActivity.java
@@ -2,133 +2,72 @@ package com.example.calculatorengineeringview;
 
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnZero;
-    private Button btnOne;
-    private Button btnTwo;
-    private Button btnThree;
-    private Button btnFour;
-    private Button btnFive;
-    private Button btnSix;
-    private Button btnSeven;
-    private Button btnEigth;
-    private Button btnNine;
-    private Button btnPoint;
-    private TextView txtOutput;
-    private String textBuffer; //TODO скинь в другую задачу
-    View.OnClickListener onButtonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Button btn = (Button) v;
-            String curBtn = btn.getText().toString();
-
-            if (TextUtils.isEmpty(txtOutput.getText())) {
-                textBuffer ="";
-            } else {
-                textBuffer = txtOutput.getText().toString();
-            }
-
-            if (curBtn.equals(getString(R.string.btn_point)) &&
-                    textBuffer.contains(getString(R.string.btn_point))) {
-                Toast.makeText(MainActivity.this,
-                        getString(R.string.error_point_already_exist), Toast.LENGTH_SHORT).show();
-            } else {
-                textBuffer += curBtn;
-            }
-
-            txtOutput.setText(textBuffer);
-
-        }
-    };
-    private Toolbar myToolbar;
-    private FrameLayout engineeringViewFrame;
+    private ConstraintLayout engineeringView;
     private CalcView curCalcView = CalcView.MAIN_VIEW;
+    private String textBuffer;
+    private TextView txtOutput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
-    }
-
-    private void initView() {
-        btnZero = findViewById(R.id.btnZero);
-        btnOne = findViewById(R.id.btnOne);
-        btnTwo = findViewById(R.id.btnTwo);
-        btnThree = findViewById(R.id.btnThree);
-        btnFour = findViewById(R.id.btnFour);
-        btnFive = findViewById(R.id.btnFive);
-        btnSix = findViewById(R.id.btnSix);
-        btnSeven = findViewById(R.id.btnSeven);
-        btnEigth = findViewById(R.id.btnEight);
-        btnNine = findViewById(R.id.btnNine);
-        btnPoint = findViewById(R.id.btnPoint);
-
+        engineeringView = findViewById(R.id.engineeringView);
+        engineeringView.setVisibility(View.GONE);
         txtOutput = findViewById(R.id.txtOutput);
 
-        myToolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-
-        engineeringViewFrame = findViewById(R.id.engineeringViewFrame);
-        engineeringViewFrame.setVisibility(View.GONE);
-
-        btnZero.setOnClickListener(onButtonClickListener);
-        btnOne.setOnClickListener(onButtonClickListener);
-        btnTwo.setOnClickListener(onButtonClickListener);
-        btnThree.setOnClickListener(onButtonClickListener);
-        btnFour.setOnClickListener(onButtonClickListener);
-        btnFive.setOnClickListener(onButtonClickListener);
-        btnSix.setOnClickListener(onButtonClickListener);
-        btnSeven.setOnClickListener(onButtonClickListener);
-        btnEigth.setOnClickListener(onButtonClickListener);
-        btnNine.setOnClickListener(onButtonClickListener);
-        btnPoint.setOnClickListener(onButtonClickListener);
-
     }
 
-    private void changeMode(MenuItem actionChangeMode) {
+    public void onClick(View v) {
+        Button btn = (Button) v;
+        String curBtn = btn.getText().toString();
+
+        if (TextUtils.isEmpty(txtOutput.getText())) {
+            textBuffer = "";
+        } else {
+            textBuffer = txtOutput.getText().toString();
+        }
+
+        if (curBtn.equals(getString(R.string.btn_point)) &&
+                textBuffer.contains(getString(R.string.btn_point))) {
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.error_point_already_exist), Toast.LENGTH_SHORT).show();
+        } else {
+            textBuffer += curBtn;
+        }
+
+        txtOutput.setText(textBuffer);
+    }
+
+    public void changeMode(View view) {
+        CharSequence curTxtInOutput;
+
         switch (curCalcView) {
             case MAIN_VIEW:
                 curCalcView = CalcView.ENGINEER_VIEW;
-                engineeringViewFrame.setVisibility(View.VISIBLE);
-                actionChangeMode.setTitle(R.string.btn_main_view);
+                engineeringView.setVisibility(View.VISIBLE);
+                curTxtInOutput = txtOutput.getText();
+                txtOutput = findViewById(R.id.txtOutputEnView);
+                txtOutput.setText(curTxtInOutput);
                 break;
 
             case ENGINEER_VIEW:
                 curCalcView = CalcView.MAIN_VIEW;
-                engineeringViewFrame.setVisibility(View.GONE);
-                actionChangeMode.setTitle(R.string.btn_engineering_view);
+                engineeringView.setVisibility(View.GONE);
+                curTxtInOutput = txtOutput.getText();
+                txtOutput = findViewById(R.id.txtOutput);
+                txtOutput.setText(curTxtInOutput);
                 break;
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.actionChangeMode:
-                changeMode(item);
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
